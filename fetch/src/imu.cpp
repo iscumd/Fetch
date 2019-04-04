e7cbf092717a970b2be5c1551c5c91b37f7defab
pellegreene <36171165+pellegreene@users.noreply.github.com>
2019-04-03 23:19:52 -0400

    Update imu.cpp
    Adding function call that hopefully assigns value to the data struct.

--- fetch/src/imu.cpp
+++ fetch/src/imu.cpp
@@ -4,7 +4,7 @@
 
 #include <string>
 
-static rc_mpu_data_t data;
+static rc_mpu_data_t data; //Data from MPU
 ros::Publisher orientationPub;
 geometry_msgs::orienation msg;
 
@@ -17,10 +17,26 @@ int main(int argc, char **argv){
 	n.param("orientation_control_enable_logging", enableLogging, false);
 	orientationPub = n.advertise<geometry_msgs::Quaternion>("orientation_control", 5);
 	
-	//Returns the euler angles in degrees
-	msg->data.x = dmp_TaitBryan[TB_PITCH_X]*RAD_TO_DEG; //Pitch
-	msg->data.y = dmp_TaitBryan[TB_ROLL_Y]*RAD_TO_DEG;//Roll
-	msg->data.z = dmp_TaitBryan[TB_YAW_Z]*RAD_TO_DEG; //Yaw
+	rc_mpu_config_t mpu_config = rc_mpu_default_config();
+   
+   /* Gonna see if we don't even need this
+   mpu_config.dmp_sample_rate = SAMPLE_RATE_HZ;
+    mpu_config.orient = ORIENTATION_Y_UP;
+	
+	//Initialize MPU
+	if(rc_mpu_initialize_dmp(&data, mpu_config)){
+                fprintf(stderr,"ERROR: can't talk to IMU, all hope is lost\n");
+                rc_led_blink(RC_LED_RED, 5, 5);
+                return -1;
+        }
+	
+	*/
+	
+	mpu_read_gyro(&data);
+	//Returns the Euler angles in degrees
+	msg->data.x = data.dmp_TaitBryan[TB_PITCH_X]*RAD_TO_DEG; //Pitch
+	msg->data.y = data.dmp_TaitBryan[TB_ROLL_Y]*RAD_TO_DEG;//Roll
+	msg->data.z = data.dmp_TaitBryan[TB_YAW_Z]*RAD_TO_DEG; //Yaw
 	
 	//Pitch = data.x 	Roll = data.y   	Yaw = data.z
 	orientationPub.publish(msg);
