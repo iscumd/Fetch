4465a239220fa89234a85cb7bf9e75e0509854e5
pellegreene <36171165+pellegreene@users.noreply.github.com>
2019-04-03 21:44:34 -0400

    Starting Gait Control
    Starting Gait Control for Orientation Control. Removing Orientation Control CPP file.

--- fetch/src/gait_control.cpp
+++ fetch/src/gait_control.cpp
@@ -1,11 +1,14 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Quaternion.h"
+#include "rc/mpu.h"
 
 #include <string>
 
 ros::Publisher gaitPub;
-
+ros::Subscriber eulerSub;
+static rc_mpu_data_t data;
 bool enableLogging;
+float32<vector> deltaRho = 0;
 
 int main(int argc, char **argv){
 	ros::init(argc, argv, "gait_control");
@@ -13,12 +16,18 @@ int main(int argc, char **argv){
 	ros::NodeHandle n;
 
 	n.param("gait_control_enable_logging", enableLogging, false);
-
-	gaitPub = n.advertise<geometry_msgs::Quaternion>("gait_control", 5);
-
-	// ros::Subscriber joystickSub = n.subscribe("joystick/xinput", 5, joystickCallback);
-
+	
+	eulerSub = n.subscribe("orientation_control", 5, orientationControlCallback);
+	
 	ros::spin();
 
 	return 0;
 }
+
+
+void orientationControlCallback(geometry_msgs::Quaternion::orientation &msg){
+	//Message reads in Pitch, Roll, Yaw(msg->data.x, msg->data.y, msg->data.z respectively)
+	//Pitch should be no greater than |30 degrees| (absolute value)
+	//Roll should be no greater than 
+	
+}	
